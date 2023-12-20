7a715f59fa1b642848adadeabd2e592a36d82e76
tardhiansyah <tardhians@gmail.com>
2023-12-20 12:18:05 +0700

    add nlog and log4net

--- Log4NetTutorial1/Program.cs
+++ Log4NetTutorial1/Program.cs
@@ -1,2 +1,29 @@
 ﻿using log4net;
+using log4net.Config;
 
+namespace LoggingExample
+{
+    class Program
+    {
+        private static readonly ILog logger = LogManager.GetLogger(typeof(Program));logger.Debug("This is a debug message");
+        static void Main()
+        {
+            XmlConfigurator.Configure(new FileInfo("log4net.config"));
+            logger.Debug("This is a debug messsage");
+            logger.Info("This is an example message");
+            logger.Warn("This is a warning message");
+            logger.Error("This is an error message");
+            logger.Fatal("This is a fatal message");
+            GameRunner.Message("Game running.");
+        }
+    }
+
+    class GameRunner
+    {
+        private static readonly ILog logger = LogManager.GetLogger(typeof(GameRunner));
+        public static void Message(string args)
+        {
+            logger.Info(args);
+        }
+    }
+}
